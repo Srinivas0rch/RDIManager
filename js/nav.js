7d99183427b293a9540a434e4338147452e0a75c
LouisPLN <louispoulinpro@gmail.com>
2021-05-26 15:00:54 +0200

    WIP : modifs

--- js/nav.js
+++ js/nav.js
@@ -8,3 +8,30 @@ window.addEventListener('scroll', () => {
         navigation.classList.remove('animNav');
     }
 })
+
+var menu = document.querySelector(".menu")
+var ham = document.querySelector(".ham")
+var xIcon = document.querySelector(".xIcon")
+var menuIcon = document.querySelector(".menuIcon")
+
+ham.addEventListener("click", toggleMenu)
+
+function toggleMenu() {
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    xIcon.style.display = "none";
+    menuIcon.style.display = "block";
+  } else {
+    menu.classList.add("showMenu");
+    xIcon.style.display = "block";
+    menuIcon.style.display = "none";
+  }
+}
+
+var menuLinks = document.querySelectorAll(".menuLink")
+
+menuLinks.forEach(
+  function (menuLink) {
+    menuLink.addEventListener("click", toggleMenu)
+  }
+)
